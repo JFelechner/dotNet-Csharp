3fe88663f0a729e9f593f1c7bd7a2198220f41e9
JFelechner <jfelechner@comcast.net>
2022-02-07 20:10:05 -0600

    first commit

---
+++ language_fundamentals/collections_practice/Program.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+namespace collections_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Three Basic Arrays
+            // Create an array to hold integer values 0 through 9
+            int[] numArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // Create an array of the names "Tim", "Martin", "Nikki", & "Sara"
+            string[] namesArray = { "Tim", "Martin", "Nikki", "Sara" };
+            // Create an array of length 10 that alternates between true and false values, starting with true
+            bool[] trueFalseArray = { true, false, true, false, true, false, true, false, true, false };
+
+
+            // List of Flavors
+            // Create a list of ice cream flavors that holds at least 5 different flavors (feel free to add more than 5!)
+            List<string> iceCreamFlavors = new List<string>();
+
+
+            // User Info Dictionary
+            // Create a dictionary that will store both string keys as well as string values
+            Dictionary<string, string> userInfo = new Dictionary<string, string>();
+
+
+
+
+            // **********************
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine(numArray);
+            // Console.WriteLine(strArray);
+            // Console.WriteLine(trueFalseArray);
+            iceCreamFlavors.Add("vanilla");
+            iceCreamFlavors.Add("chocolate");
+            iceCreamFlavors.Add("butterscotch");
+            iceCreamFlavors.Add("fudge");
+            iceCreamFlavors.Add("orange");
+
+            // Output the length of this list after building it
+            Console.WriteLine("Here are the ice creame flavors");
+            for (var i = 0; i < iceCreamFlavors.Count; i++)
+            {
+                Console.WriteLine("-" + iceCreamFlavors[i]);
+            }
+
+            // Output the third flavor in the list, then remove this value
+            Console.WriteLine("Flavor 3: " + iceCreamFlavors[2]);
+            iceCreamFlavors.Remove(iceCreamFlavors[2]);
+
+            // Output the new length of the list (It should just be one fewer!)
+            Console.WriteLine("Here are the updated ice creame flavors");
+            for (var i = 0; i < iceCreamFlavors.Count; i++)
+            {
+                Console.WriteLine("-" + iceCreamFlavors[i]);
+            }
+
+            // Add key/value pairs to this dictionary where:
+            // each key is a name from your names array
+            // each value is a randomly select a flavor from your flavors list.
+            userInfo.Add(namesArray[0], iceCreamFlavors[0]);
+            userInfo.Add(namesArray[1], iceCreamFlavors[1]);
+            userInfo.Add(namesArray[2], iceCreamFlavors[2]);
+            userInfo.Add(namesArray[3], iceCreamFlavors[3]);
+
+            // Loop through the dictionary and print out each user's name and their associated ice cream flavor
+            foreach (KeyValuePair<string, string> entry in userInfo)
+            {
+                Console.WriteLine(entry.Key + " - " + entry.Value);
+            }
+
+        }
+    };
+}
--- language_fundamentals/fundamentals/Program.cs
+++ language_fundamentals/fundamentals/Program.cs
@@ -25,7 +25,7 @@ namespace fundamentals
         {
             for (int i = 1; i <= 100; i++)
             {
-                if(i % 3 == 0 && i % 5 == 0)
+                if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine(i);
                 }
@@ -37,15 +37,15 @@ namespace fundamentals
         {
             for (int i = 1; i <= 100; i++)
             {
-                if(i % 3 == 0 && i % 5 == 0)
+                if (i % 3 == 0 && i % 5 == 0)
                 {
                     Console.WriteLine("FizzBuzz");
                 }
-                else if(i % 3 == 0)
+                else if (i % 3 == 0)
                 {
                     Console.WriteLine("Fizz");
                 }
-                else if(i % 5 == 0)
+                else if (i % 5 == 0)
                 {
                     Console.WriteLine("Buzz");
                 }
@@ -54,6 +54,39 @@ namespace fundamentals
                     Console.WriteLine(i);
                 }
             }
+
+            // // Create a new loop that prints all values from 1-100 that are divisible by 3 or 5, but not both
+            // for (var value = 1; value <= 100; value++)
+            // {
+            //     bool byThreeOrFive = (value % 3 == 0 || value % 5 == 0);
+            //     bool notThreeAndFive = !(value % 3 == 0 && value % 5 == 0);
+
+            //     if (byThreeOrFive && notThreeAndFive)
+            //     {
+            //         Console.WriteLine(value);
+            //     }
+            // }
+
+            // // Modify the previous loop to print "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5
+            // for (var value = 1; value <= 100; value++)
+            // {
+            //     bool byThree = (value % 3 == 0);
+            //     bool byFive = (value % 5 == 0);
+            //     bool byThreeAndFive = (value % 3 == 0 && value % 5 == 0);
+
+            //     if (byThreeAndFive)
+            //     {
+            //         Console.WriteLine($"Fizz Buzz (value:{value})");
+            //     }
+            //     else if (byThree)
+            //     {
+            //         Console.WriteLine($"Fizz (value:{value})");
+            //     }
+            //     else if (byFive)
+            //     {
+            //         Console.WriteLine($"Buzz (value:{value})");
+            //     }
+            // }
         }
     }
 }
