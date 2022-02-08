a983f88891380b090cd7a9e905ca3a001e297090
JFelechner <jfelechner@comcast.net>
2022-02-07 18:10:05 -0600

    first commit

---
+++ language_fundamentals/fundamentals/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            // Print1to255();
+            // Div3or5();
+            FizzBuzz();
+        }
+
+        public static void Print1to255()
+        {
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        // Create a new loop that prints all values from 1-100 that are divisible by 3 or 5
+        public static void Div3or5()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        // Modify the previous loop to print "Fizz" for multiples of 3, "Buzz" for multiples of 5, and "FizzBuzz" for numbers that are multiples of both 3 and 5
+        public static void FizzBuzz()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
